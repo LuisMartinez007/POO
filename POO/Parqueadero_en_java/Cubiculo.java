ebc313faba56c63301c119369aea5b180c9f3ed7
LuisMartinez007 <159143417+LuisMartinez007@users.noreply.github.com>
2024-08-13 08:15:04 -0500

    Update Cubiculo.java

--- POO/Parqueadero_en_java/Cubiculo.java
+++ POO/Parqueadero_en_java/Cubiculo.java
@@ -3,7 +3,7 @@ package POO.Parqueadero_en_java;
 public class Cubiculo {
     public String id;
     public String numero;
-    private double ancho;
-    private double largo;
+    public double ancho;
+    public double largo;
     public String tipo;
 }
